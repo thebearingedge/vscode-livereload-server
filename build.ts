aa473723eceadd32933534068bf5d6de1549ad54
Tim Davis <contact@timdav.is>
2022-05-25 17:26:42 -0700

    skip copying public serve-index

--- build.ts
+++ build.ts
@@ -1,12 +1,7 @@
-import util from 'util'
-import path from 'path'
-import ncp from 'ncp'
 import esbuild from 'esbuild'
 import yargs from 'yargs-parser'
 
 const { _, ...argv } = yargs(process.argv.slice(2)) ?? {}
-
-const copyDir = util.promisify(ncp)
 
 void esbuild
   .build({
@@ -26,7 +21,6 @@ void esbuild
     ],
     ...argv
   })
-  .then(() => copyDir(path.join(require.resolve('serve-index'), '..', 'public'), './dist/public'))
   .catch(err => {
     console.error(err)
     process.exit(1)
